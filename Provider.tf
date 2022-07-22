fed094318af18356e3ca0344ba50f07083072ffb
craig.holmes <craig.holmes@lvs1.com>
2022-07-22 23:20:51 +0000

    fix: update module

--- Provider.tf
+++ Provider.tf
@@ -4,20 +4,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 2.88.1"
+      version = ">= 2.88.1"
     }
   }
 }
 
-provider "azurerm" {
+# provider "azurerm" {
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+#   subscription_id = var.subscription_id
+#   tenant_id       = var.tenant_id
 
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
+#   features {
+#     key_vault {
+#       purge_soft_delete_on_destroy = true
+#     }
+#   }
 
-}
+# }

a6b7a31729874a0a04af6026cc08feb09dd0e132
craig.holmes <craig.holmes@lvs1.com>
2022-07-22 23:25:22 +0000

    fix: move ps1 script

--- WindowsExtension-InitializeDisks.tf
+++ WindowsExtension-InitializeDisks.tf
@@ -1,7 +1,7 @@
 module "InitializeDisks" {
   source             = "./modules/WindowsScriptExtension_v1"
   scriptName         = "InitializeDisks.ps1" #The name of the script to be run. Path and name must be seperate."
-  scriptPath         = "scripts"             #"The path of the script to be run.  Path and name must be seperate."
+  scriptPath         = "${path.module}/modules/WindowsScriptExtension_v1"             #"The path of the script to be run.  Path and name must be seperate."
   virtual_machine_id = azurerm_windows_virtual_machine.main.id
 
   depends_on = [

e43e628dcdc09b5758bb51043814ca39f8d4bfe8
craig.holmes <craig.holmes@lvs1.com>
2022-07-25 20:41:38 +0000

    feat: add 3rd vm

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -30,9 +30,8 @@ module "vm_test_1" {
       lunID             = 13
     },
   }
-  tags = {
-    first = "First tag Windows VM"
-  }
+  
+  tags = var.tags
   admin_username = "adminusername"
 }
 
@@ -52,8 +51,27 @@ module "vm_test_2" {
   store_admin_password_in_KV = true
   admin_password_kv_id = azurerm_key_vault.fixture.id
   data_disk_details = {}
-  tags = {
-    first = "First tag Windows VM"
-  }
+  tags = var.tags
+  admin_username = "adminusername"
+}
+
+
+module "vm_test_2b" {
+  source = "../../"
+  resource_group_name = azurerm_resource_group.fixture.name
+
+  admin_password             = "" #blank means randomize it in the module.
+  subnet_id                  = azurerm_subnet.fixture_sn1.id
+  backup_vault_enabled       = false
+  use_dynamic_plan           = false
+  vm_name                    = "vm-win-test2b"
+  availability_set_id        = azurerm_availability_set.fixture.id
+  image_URN                  = "MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest"
+  Encrpyt_all_VM_Disks       = false
+  vm_size                    = "Standard_D2s_v5"
+  store_admin_password_in_KV = true
+  admin_password_kv_id = azurerm_key_vault.fixture.id
+  data_disk_details = {}
+  tags = var.tags
   admin_username = "adminusername"
 }

8e95bd4b0d8aa33acf0dc818baca4c7f61cf7bba
craig.holmes <craig.holmes@lvs1.com>
2023-05-08 17:00:07 -0600

    feat: update providers

--- modules/addDisks_v1/providers.tf
+++ modules/addDisks_v1/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.88.1"
+      version = ">=3.0.0, <4.0.0"
     }
   }
 }
--- providers.tf
+++ providers.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.99, <3.0.0"
+      version = ">=3.0.0, <4.0.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "3.1.1"
+      version = ">=3.1.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.3.2"
+      version = ">=3.3.0"
     }
   }
 }
